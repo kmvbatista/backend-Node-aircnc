1d8d260900699d87437a67224ad5f7b28c133c08
Kennedy <kennedymnovas@gmail.com>
2019-10-12 20:40:36 -0300

    BookingModel

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const multer = require('multer');
 const uploadConfig = require('./config/upload');
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
 
 const routes  = express.Router();
 const upload = multer(uploadConfig);
@@ -10,5 +11,6 @@ const upload = multer(uploadConfig);
 routes.post('/users', SessionController.store);
 routes.post('/spots',upload.single('thumbnail'), SpotController.store);
 routes.get('/spots', SpotController.index);
+routes.get('/dashboard', DashboardController.show)
 
 module.exports= routes;
